ffa051b7664109d7267c28e68e9439415ad4a3d4
prateek3189 <prateek.magarde@gmail.com>
2015-04-20 01:33:31 +0530

    First Commit for pm-lib

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE>
+<html>
+<head>
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript" src="pm-lib-min.js"></script>
+<link type="text/css" rel="stylesheet" href="pm-lib-min.css"/>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("#S1").customized();
+		$("#S2").customized();
+	});
+</script>
+</head>
+<body>
+<select id="S1">
+	<option value="">Select</option>
+	<option value="V11" >Val11</option>
+	<option value="V12">Val12</option>
+	<option value="V13">Val13</option>
+	<option value="V14"selected="selected">Val14</option>
+</select>
+<br/>
+<select id="S2">
+	<option value="">Select</option>
+	<option value="V11">Val11</option>
+	<option value="V12">Val12</option>
+	<option value="V13" selected="selected">Val13</option>
+	<option value="V14">Val14</option>
+</select>
+
+
+
+</body>
+</html>
---
+++ pm-lib.js
@@ -0,0 +1,61 @@
+$(function(){
+	$.fn.extend({
+		customized: function() {
+			var randStr	=	Math.random().toString().substr(2,8);
+			var thisID		=	this.addClass("pm_"+randStr);
+			var thisWidth	=	this.css("width");
+			var selectHTML	=	'';
+			selectHTML += '<div class="generateSelect" id="select_'+randStr+'"><span class="generateTitle" id="generated_'+randStr+'" dir="'+randStr+'">Select</span><ul class="generatSelectors" dir="'+randStr+'" id="selector_'+randStr+'">';
+			var index	=	0;
+			var selectTitle	=	'';
+			$(this).children("option").each(function(){
+				var liVal		=	$(this).val();
+				var liHTML		=	$(this).html();
+				selectHTML	+=	'<li dir="'+liVal+'" value="'+index+'"';
+				if($(this).prop("selected")){
+					selectHTML	+=	'class="selected"';	
+					selectTitle	=	liHTML;
+				}
+				selectHTML	+=	'>'+liHTML+'</li>';
+				index++;				
+			});
+			selectHTML	+=	'</ul></div>';
+			$(".pm_"+randStr).after(selectHTML);
+			if(selectTitle != ''){
+				$("#generated_"+randStr).html(selectTitle);
+			}
+			$(".pm_"+randStr).hide();
+		}
+	});
+	$(document).on('click', '.generateTitle', function(e){
+ 		e.stopPropagation();
+		$('.generatSelectors').hide();
+		var thisID			=	$(this).attr("dir");
+		var thisSelector	=	$("#selector_"+thisID);
+		if(thisSelector.is(":visible")){
+			thisSelector.hide();
+		}else{
+			thisSelector.show();
+		}
+		var scrollPos	=	isNaN(parseInt($('.generatSelectors li.selected').val())) ? 0 : parseInt($('.generatSelectors li.selected').val());
+		var multiple	=	40;
+		scrollPos		=	parseInt(scrollPos*multiple);
+		thisSelector.scrollTop(scrollPos);
+	});
+	
+	$(document).on('click', '.generatSelectors li', function(e){
+		e.stopPropagation();
+		var selectorID	=	$(this).parent().attr("dir");
+		$('#selector_'+selectorID+' li').removeClass("selected");
+		$(this).addClass("selected");
+		var getVal	=	$(this).attr("dir");
+		var getName	=	$(this).html();
+		$(".pm_"+selectorID).val(getVal);
+		$("#generated_"+selectorID).html(getName);
+		$(".generatSelectors").hide();
+	});
+
+});
+$(document).on("click",function(){
+	$('.generatSelectors').hide();
+});
